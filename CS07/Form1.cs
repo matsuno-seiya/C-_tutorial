924a58c4555a5692fa6120d7862acfc6c272249b
matsuno-seiya <seiya.matsuno@nsw.co.jp>
2025-06-06 12:01:13 +0900

    変数と定数の内容

---
+++ CS07/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS07
+{
+    public partial class Form1 : Form
+    {
+        private int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            count = count +  1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(count.ToString());
+        }
+    }
+}
