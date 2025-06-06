23e79b61407fee1f7ad46325002661c4112806a5
matsuno-seiya <seiya.matsuno@nsw.co.jp>
2025-06-06 14:42:36 +0900

    値型と参照型の内容

---
+++ CS09/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CS09
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.値型 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // 値型
+            // 
+            this.値型.Location = new System.Drawing.Point(289, 136);
+            this.値型.Name = "値型";
+            this.値型.Size = new System.Drawing.Size(213, 39);
+            this.値型.TabIndex = 0;
+            this.値型.Text = "値型";
+            this.値型.UseVisualStyleBackColor = true;
+            this.値型.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(282, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "参照型";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 294);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(252, 79);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "参照型引数あり";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.値型);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button 値型;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CS09/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            byte byteValue = 255;
+
+            short shortMax = short.MaxValue;
+            ushort ushortMax = ushort.MaxValue; //マイナスなしはushort
+
+            uint uintMax = uint.MaxValue;
+
+            ulong ulongMax = ulong.MaxValue;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 10;
+            int b = 20;
+
+            MessageBox.Show("a=" + a);
+            MessageBox.Show("b=" + b);
+
+            a = b;
+
+            MessageBox.Show("a=" + a);
+            MessageBox.Show("b=" + b);
+
+            b = 30;
+
+            MessageBox.Show("a=" + a);
+            MessageBox.Show("b=" + b);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Other a = new Other();
+            Other b = new Other();
+
+            a.Value = 10;
+            b.Value = 20;
+
+            MessageBox.Show("a = " + a.Value);
+            MessageBox.Show("b =" + b.Value);
+
+            a = b;
+
+            MessageBox.Show("a = " + a.Value);
+            MessageBox.Show("b =" + b.Value);
+
+            b.Value = 30;
+
+            MessageBox.Show("a = " + a.Value);
+            MessageBox.Show("b =" + b.Value);
+        }
+
+        private void OtherMethod(Other other)
+        {
+            other.Value = 55;
+         }   
+        public class Other
+        {
+            public int Value;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Other a = new Other();
+            a.Value = 10;
+            OtherMethod(a);
+            MessageBox.Show("a=" + a.Value);
+        }
+
+
+        
+    }
+}
