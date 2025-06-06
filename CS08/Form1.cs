f82ba3af1f606a789c798dfd49b9d3e8e3c3109f
matsuno-seiya <seiya.matsuno@nsw.co.jp>
2025-06-06 13:17:07 +0900

    データの型の内容

---
+++ CS08/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS08
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            byte byteValue = 255;
+            //byte byteValue2 = 256;
+
+            short shortvalue = 10;
+            short shortMax = short.MaxValue;
+
+            int intValue = int.MaxValue; //MaxValueにすることで最大の数が入る
+
+            long longValue = long.MaxValue;
+
+            float floatValue = float.MaxValue;
+            double doubleValue = double.MaxValue;
+            decimal decimalValue = decimal.MaxValue;
+
+            bool doolValue = true;
+
+
+            string stringValue = "はい";
+
+            if (doolValue == true) {
+               
+            }
+            else { 
+            
+            }
+
+     
+
+
+
+
+        }
+
+
+
+    }
+}
